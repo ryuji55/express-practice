292608338bee30a0972820b5bd2bbfd9c5e451ad
ryuji55 <rt.october.2@icloud.com>
2023-03-26 19:47:45 +0900

    Add:usersページ作成・nodemon設定

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,7 +3,22 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("respond with a resource");
+  const name = req.query.name;
+  const data = {
+    title: "usersPage",
+    content: "これは、test。" + name,
+  };
+
+  res.render("users", data);
+});
+
+router.post("/post", (req: Request, res: Response, next: NextFunction) => {
+  const msg = req.body["message"];
+  const data = {
+    title: "usersPage",
+    content: "あなたは" + msg + "と送信しました",
+  };
+  res.render("users", data);
 });
 
 export { router as usersRouter };
