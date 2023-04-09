31d9476826918d55de0952174dd0fffa7dbee541
ryuji55 <rt.october.2@icloud.com>
2023-04-09 19:27:14 +0900

    Add:react-router

--- frontend/src/components/users/Users.tsx
+++ frontend/src/components/users/Users.tsx
@@ -1,6 +1,9 @@
-import React, { memo } from "react";
+import { memo, useCallback } from "react";
 import { useForm } from "react-hook-form";
-import axios from "axios";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/system";
+import { Link } from "react-router-dom";
+import createFormData from "../../methods/apis/createFormData";
 
 type FormData = {
   name: string;
@@ -15,36 +18,50 @@ const Users = memo(() => {
   } = useForm<FormData>();
 
   const onSubmit = async (data: FormData) => {
-    try {
-      const response = await axios.post("/users/submit-form", data);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
+    createFormData(data);
   };
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          {...register("name", { required: "Name is required" })}
-          id="name"
-        />
-        {errors.name && <span>{errors.name.message}</span>}
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          {...register("email", { required: "Email is required" })}
-          id="email"
-        />
-        {errors.email && <span>{errors.email.message}</span>}
-      </div>
+  const handleClick = useCallback(() => {
+    console.log("初めてのMUI Button");
+  }, []);
 
-      <button type="submit">Submit</button>
-    </form>
+  return (
+    <Wrapper>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            {...register("name", { required: "Name is required" })}
+            id="name"
+          />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            {...register("email", { required: "Email is required" })}
+            id="email"
+          />
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <StyledButton>
+        <Button variant="contained" color="primary" onClick={handleClick}>
+          MUI Button
+        </Button>
+      </StyledButton>
+      <Link to="/">Topに戻る</Link>
+    </Wrapper>
   );
 });
+
+const StyledButton = styled("div")`
+  margin: 20px 0;
+`;
+
+const Wrapper = styled("div")`
+  text-align: center;
+`;
 
 export default Users;
